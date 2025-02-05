d597fbfd4abbbc4e7ca8a53a2b1cc1ac246a9330
LucasP <pautzl19@gmail.com>
2025-02-05 14:08:33 -0600

    Add files via upload

---
+++ TheSistersAndDuckbear/Program.cs
@@ -0,0 +1,18 @@
+﻿//Written by Lucas Pautz
+//1-29-2025
+namespace TheSistersAndDuckbear
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+
+            SisterAndDuckBear aSisterBear = new SisterAndDuckBear();
+            Console.Write("Input the number of chocolate eggs to be dispersed: ");
+            aSisterBear.ChocEggs = int.Parse(Console.ReadLine());
+            Console.WriteLine(aSisterBear.ToString());
+            //  some scenarios where the duckbear gets more choclate eggs is where there are 1,2,3,6,7, or 11 eggs
+        }
+    }
+}
---
+++ TheSistersAndDuckbear/SisterAndDuckBear.cs
@@ -0,0 +1,44 @@
+﻿//Written by Lucas Pautz
+//1-29-2025
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSistersAndDuckbear
+{
+    public class SisterAndDuckBear
+    {
+        //class variables
+        private int chocEggs = -1;
+
+
+        //gets and sets
+        public int ChocEggs
+        {
+            get; set;
+        }
+
+
+        //methods
+
+        public int EggsPerSister()
+        {
+            int eggsPerSister = ChocEggs / 4;
+            return eggsPerSister;
+        }
+        public int EggsForDuckBear()
+        {
+            int eggsForDuckBear = ChocEggs % 4;
+            return eggsForDuckBear;
+        }
+
+        public override string ToString()
+        {
+            string msg = "";
+            msg += $"Each sister gets {EggsPerSister()} cocolate eggs \nThe DuckBear gets {EggsForDuckBear()} chocolate eggs";
+            return msg;
+        }
+    }
+}
