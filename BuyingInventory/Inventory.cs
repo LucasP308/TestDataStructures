31b4f760b0b8baf82d4b981380a589a4be7273d2
LucasP <pautzl19@gmail.com>
2025-02-06 16:55:43 -0600

    Add files via upload

---
+++ BuyingInventory/Inventory.cs
@@ -0,0 +1,61 @@
+﻿//written by Lucas Pautz
+//2-5-2025
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyingInventory
+{
+    public  class Inventory
+    {//class variables
+        private int invNum = -1;
+        
+
+
+        //gets and sets
+        public int InvNum
+        {
+            get;set;
+        }
+        
+
+        //methods
+
+        public string getPrice()
+        {
+            int num = InvNum;
+            switch (InvNum)
+            {
+                case 1:
+                    return "Rope costs 10 gold.";
+                case 2:
+                    return "Torches cost 15 gold.";
+                case 3:
+                    return "CLimbing Equipment costs 25 gold.";
+                case 4:
+                    return "Clean Water cost 1 gold.";
+                case 5:
+                    return "A Machete costs 20 gold.";
+                case 6:
+                    return "A Canoe cost 200 gold.";
+                case 7:
+                    return "Food Supplies costs 1 gold.";
+                default:
+                    return "Enter a number from 1-7";
+            }
+        }
+        
+
+        public override string ToString()
+        {
+            string msg = getPrice();
+            return msg;
+
+        }
+
+          
+    }
+}
---
+++ BuyingInventory/Program.cs
@@ -0,0 +1,25 @@
+﻿//Written by Lucas Pautz
+//2-5-2025
+namespace BuyingInventory
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Inventory aInventory = new Inventory();
+            Console.WriteLine("The following items are available:");
+            Console.WriteLine("1-Rope");
+            Console.WriteLine("2-Torches");
+            Console.WriteLine("3-Climbing Equipment");
+            Console.WriteLine("4-Clean Water");
+            Console.WriteLine("5-Machete");
+            Console.WriteLine("6-Canoe");
+            Console.WriteLine("7-Food Supplies");
+            Console.Write("What number would you like to see the price of?: ");
+            aInventory.InvNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(aInventory.ToString());
+
+
+        }
+    }
+}
