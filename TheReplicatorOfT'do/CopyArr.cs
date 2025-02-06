9368c8f76356aec41499817df5c481f3d5b5b9b0
LucasP <pautzl19@gmail.com>
2025-02-06 16:58:37 -0600

    Add files via upload

---
+++ TheReplicatorOfT'do/CopyArr.cs
@@ -0,0 +1,54 @@
+﻿//Written by Lucas Pautz
+//2-6-2025
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheReplicatorOfT_do
+{
+    public class CopyArr
+    {// class variables
+        private int[] originalArr = new int [5];
+        
+
+        //methods
+        public int[] OriginalArr
+        {
+            get {  return this.originalArr; }
+            set { this.originalArr = value; }
+        }
+
+        public int[] CopyArray()
+        {
+            int[] copy = new int[5];
+            for (int i = 0; i < OriginalArr.Length; i++)
+            {
+                copy[i] = OriginalArr[i];
+            }
+            return copy;
+                
+        }
+
+        public override string ToString()
+        {
+            string msg = "";
+            int[] arr = CopyArray();
+            msg += "Orignal Array: ";
+            foreach (int i in OriginalArr)
+            {
+                msg += i + " ";
+            }
+            msg += "\n";
+            msg += "Copied Array: ";
+            foreach (int i in arr)
+            {
+                msg += i + " ";
+            }
+            return msg;
+        }
+
+    }
+}
---
+++ TheReplicatorOfT'do/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TheReplicatorOfT_do
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CopyArr aCopyArr = new CopyArr();
+
+            Console.WriteLine("Enter 5 integers");
+            for (int i = 0;i<5; i++)
+            {
+                aCopyArr.OriginalArr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine(aCopyArr.ToString());
+        }
+    }
+}
